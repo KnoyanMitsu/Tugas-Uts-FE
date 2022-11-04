4f3aa65e5d3ed4f0952f483954cec5dfa3916271
KnoyanMitsu <yanappuowo@yandex.com>
2022-11-04 09:42:15 +0700

    get

--- Kirim.php
+++ Kirim.php
@@ -1,11 +1,11 @@
 <?php
- $tanggal =   $_POST['Date'];
- $nama =  $_POST['Name'];
- $telepon =   $_POST['Phone'];
- $pesan =   $_POST['Message'];
- $email =   $_POST['Email'];
- $checkbox = $_POST['checkbox'];
- $radio = $_POST['radiop'];
+ $tanggal =   $_GET['Date'];
+ $nama =  $_GET['Name'];
+ $telepon =   $_GET['Phone'];
+ $pesan =   $_GET['Message'];
+ $email =   $_GET['Email'];
+ $checkbox = $_GET['checkbox'];
+ $radio = $_GET['radiop'];
 
     echo "Nama: $nama <br> Tanggal: $tanggal <br> Telepon: $telepon <br> Pesan: $pesan <br> Email: $email <br> Category: $checkbox <br> Tipe Breakfast: $radio"
 
